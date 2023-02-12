0178aa888c9a5bed2d6af46e0256c2c362a0131c
ZiogasThlem <ziogas.thlem@gmail.com>
2023-02-12 20:38:33 +0200

    some more styling done. added bg-image too

--- scripts/bank.js
+++ scripts/bank.js
@@ -18,33 +18,39 @@ export function setLoan(loan) {
 }
 
 const getNewLoan = () => {
-
-  if (parseInt(document.getElementById("loan-balance").innerHTML) > 0) {
-    alert(
-      "You already have an outstanding loan. Pay it back first to be elgible for a new one."
-    );
-  } 
-  else {
-    const desiredLoan = parseInt(
-      prompt("Please enter the amount you desire: ")
-    );
-    if (desiredLoan > 2 * document.getElementById("bank-balance").innerHTML) {
+  if (parseInt(document.getElementById("bank-balance").innerHTML) === 0) {
+    alert(`You don't have any balance left thus you can't take a loan`);
+  } else {
+    if (parseInt(document.getElementById("loan-balance").innerHTML) > 0) {
       alert(
-        `Your balance is ${
-          desiredLoan - 2 * document.getElementById("bank-balance").innerHTML
-        }$ too short for the chosen loan.`
+        "You already have an outstanding loan. Pay it back first to be elgible for a new one."
       );
     } else {
-      alert(`${desiredLoan}$ have been added to your balance`);
-      setLoan(
-          parseInt(document.getElementById("loan-balance").innerHTML)
-         + desiredLoan)
-      ;
-      repayLoanButton.disabled = false;
-      setBalance(
-        parseInt(document.getElementById("bank-balance").innerHTML) +
-          desiredLoan
+      const desiredLoan = parseInt(
+        prompt("Please enter the amount you desire: ")
       );
+
+      if (desiredLoan > 2 * document.getElementById("bank-balance").innerHTML) {
+        alert(
+          `Your balance is ${
+            desiredLoan - 2 * document.getElementById("bank-balance").innerHTML
+          }$ too short for the chosen loan.`
+        );
+      } else if (desiredLoan === 0)
+        alert(`You added 0$ to your balance. Yeah...`);
+      else {
+        alert(`${desiredLoan}$ have been added to your balance`);
+        setLoan(
+          parseInt(document.getElementById("loan-balance").innerHTML) +
+            desiredLoan
+        );
+
+        setBalance(
+          parseInt(document.getElementById("bank-balance").innerHTML) +
+            desiredLoan
+        );
+        if (desiredLoan != 0) repayLoanButton.disabled = false;
+      }
     }
   }
 };
--- scripts/work.js
+++ scripts/work.js
@@ -1,83 +1,84 @@
-import {setLoan, setBalance} from "./bank.js";
+import { setLoan, setBalance } from "./bank.js";
 
 const bankButton = document.getElementById("bank-btn");
 const workButton = document.getElementById("work-btn");
 const repayLoanButton = document.getElementById("repay-btn");
 const workBalanceElement = document.getElementById("work-balance");
 
-
 let workBalance = parseInt(workBalanceElement.innerHTML);
-
 
 function setWorkBalance(balance) {
   document.getElementById("work-balance").innerText = balance;
 }
 
 const handleSalaryAddition = () => {
-  setWorkBalance(workBalance+=100);
+  setWorkBalance((workBalance += 100));
 };
 
 const handlePayLoanDose = () => {
-    let loan = parseInt(document.getElementById("loan-balance").innerHTML);
-    let depositedBalance = parseInt(document.getElementById("bank-balance").innerHTML);
+  let loan = parseInt(document.getElementById("loan-balance").innerHTML);
+  let depositedBalance = parseInt(
+    document.getElementById("bank-balance").innerHTML
+  );
 
-    let transfer = parseInt(prompt("Enter your desired amount: "))
-    while (transfer > workBalance) {
-        transfer = parseInt(
-          prompt(
-            `Your balance is ${
-              transfer - workBalance
-            } short for the amount you entered. Please select a lower amount : `
-          )
-        );
-      }
-    if (loan>0){
-        if (loan < (workBalance - workBalance*(10/100))){
-            workBalance -= loan
-            transfer = workBalance
-            loan = 0
-            setLoan(loan)
-            repayLoanButton.disabled = true;
-        }
-        else{
-            loan -= workBalance*(10/100);
-            workBalance -= workBalance*(10/100);
-            transfer -= transfer*(10/100)
-            setLoan(loan)
-        }
+  let transfer = parseInt(prompt("Enter your desired amount: "));
+  while (transfer > workBalance && typeof transfer === "number") {
+    transfer = parseInt(
+      prompt(
+        `Your balance is ${
+          transfer - workBalance
+        } short for the amount you entered. Please select a lower amount : `
+      )
+    );
+  }
+  if (loan > 0) {
+    if (loan <= workBalance - workBalance * (10 / 100)) {
+      workBalance -= loan;
+      transfer = workBalance;
+      loan = 0;
+      setLoan(loan);
+      repayLoanButton.disabled = true;
+      alert(`Your loan is paid off!`);
+    } else {
+      loan -= workBalance * (10 / 100);
+      alert(
+        `${workBalance * (10 / 100)}$ have been transfered to the loan payment.`
+      );
+      workBalance -= workBalance * (10 / 100);
+      transfer -= transfer * (10 / 100);
+      setLoan(loan);
     }
+  }
 
-    depositedBalance += transfer
-    setBalance(depositedBalance)
-    workBalance -= transfer
-    document.getElementById("work-balance").innerHTML = workBalance;
-}
+  depositedBalance += transfer;
+  setBalance(depositedBalance);
+  workBalance -= transfer;
+  document.getElementById("work-balance").innerHTML = workBalance;
+  alert(`${transfer}$ have been trasfered to your bank account.`);
+};
 
 const handleRepayLoan = () => {
-    
-    let loan = parseInt(document.getElementById("loan-balance").innerHTML);
-    let depositedBalance = parseInt(document.getElementById("bank-balance").innerHTML);
+  let loan = parseInt(document.getElementById("loan-balance").innerHTML);
+  let depositedBalance = parseInt(
+    document.getElementById("bank-balance").innerHTML
+  );
 
-    if (loan >= workBalance){
-        loan -= workBalance
-        setLoan(loan)
-        workBalance = 0    
-        document.getElementById("work-balance").innerHTML = workBalance;
-    }
-    else{
-        workBalance -= loan
-        loan = 0
-        setLoan(loan)
-        depositedBalance += workBalance
-        setBalance(depositedBalance)
-        workBalance = 0
-        document.getElementById("work-balance").innerHTML = workBalance;
-    }
-}
-
+  if (loan >= workBalance) {
+    loan -= workBalance;
+    setLoan(loan);
+    workBalance = 0;
+    document.getElementById("work-balance").innerHTML = workBalance;
+  } else {
+    workBalance -= loan;
+    loan = 0;
+    setLoan(loan);
+    depositedBalance += workBalance;
+    setBalance(depositedBalance);
+    workBalance = 0;
+    document.getElementById("work-balance").innerHTML = workBalance;
+  }
+};
 
 workButton.addEventListener("click", handleSalaryAddition);
 bankButton.addEventListener("click", handlePayLoanDose);
-repayLoanButton.addEventListener("click", handleRepayLoan)
-
-
+repayLoanButton.addEventListener("click", handleRepayLoan);
